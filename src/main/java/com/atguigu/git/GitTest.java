c12d302023137754f596e1d4ac3d02e90b22fe93
daozhi_global <1820209805@qq.com>
2018-01-12 20:56:28 +0800

    4

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -3,8 +3,7 @@ package com.atguigu.git;
 public class GitTest {
 
 	public static void main(String[] args) {
-		System.err.println("main:this is first test of git for kepler");
-		System.err.println("main:this is second222 test of git for kepler");
+		System.out.println("AA");
 	}
 
 }
