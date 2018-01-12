ad8386f73324f30bdb196fd92bc6ac53f1edb0a5
daozhi_global <1820209805@qq.com>
2018-01-12 20:02:55 +0800

    kepler git first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,9 @@
+package com.atguigu.git;
+
+public class GitTest {
+
+	public static void main(String[] args) {
+		System.err.println("main:this is first test of git for kepler");
+	}
+
+}
---
+++ src/test/java/com/stguigu/git/test/GitTest.java
@@ -0,0 +1,12 @@
+package com.stguigu.git.test;
+
+import org.junit.Test;
+
+public class GitTest {
+
+	@Test
+	public void testForGit() {
+		System.err.println("@test:this is first test of git for kepler");
+	}
+
+}
