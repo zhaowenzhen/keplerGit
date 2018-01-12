7bc37627f5ed04c96a0083ac4915469e2258f5e8
daozhi_global <1820209805@qq.com>
2018-01-12 20:40:47 +0800

    this is clone commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -4,6 +4,7 @@ public class GitTest {
 
 	public static void main(String[] args) {
 		System.err.println("main:this is first test of git for kepler");
+		System.err.println("main:this is second222 test of git for kepler");
 	}
 
 }
