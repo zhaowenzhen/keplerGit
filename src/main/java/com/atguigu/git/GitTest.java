9161359b6e433829cbdda0fdc1b2c9d6525a6982
daozhi_global <1820209805@qq.com>
2018-01-13 08:39:11 +0800

    conflet test

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -7,6 +7,7 @@ public class GitTest {
 		System.out.println("Aaaaaa");
 		System.out.println("Bbbbbb");
 		System.out.println("this is first branch");		
+		System.out.println("this is a sunny day");		
 	}
 
 }
